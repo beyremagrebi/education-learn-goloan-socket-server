a49aad00b1b8cfc0b10532601e8a85b962227350
medazizguennichi <124068781+med-aziz-guennichi@users.noreply.github.com>
2025-02-12 08:42:30 +0100

    fix

--- web/web.go
+++ web/web.go
@@ -1,16 +1,12 @@
 package web
 
 import (
-	"fmt"
-
 	socketio "github.com/googollee/go-socket.io"
 )
 
 // RegisterEvents registers web-related events
 func RegisterEvents(server *socketio.Server) {
 	server.OnEvent("/", "send-meet-notification", func(s socketio.Conn, users any) {
-		fmt.Printf("Hello meet notification: %v\n", users)
-		s.Emit("send-meet-notification", users)
 		server.BroadcastToNamespace("/", "send-meet-notification", users)
 	})
 }
