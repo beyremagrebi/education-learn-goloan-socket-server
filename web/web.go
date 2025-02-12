533ef2e708fba453a766a2d8e1460c740b83c2af
medazizguennichi <124068781+med-aziz-guennichi@users.noreply.github.com>
2025-02-12 09:35:30 +0100

    fix web

--- web/web.go
+++ web/web.go
@@ -1,12 +1,23 @@
 package web
 
 import (
+	"fmt"
+
 	socketio "github.com/googollee/go-socket.io"
 )
 
 // RegisterEvents registers web-related events
 func RegisterEvents(server *socketio.Server) {
+	server.OnEvent("/", "join", func(s socketio.Conn, room string) {
+		fmt.Printf("User joined to room %s\n", room)
+		s.Join(room)
+	})
+
 	server.OnEvent("/", "send-meet-notification", func(s socketio.Conn, users any) {
-		server.BroadcastToNamespace("/", "send-meet-notification", users)
+		server.BroadcastToRoom("/", "studiffy", "send-meet-notification", users)
+	})
+	// Handle "accesDeniedForRule" event
+	server.OnEvent("/", "accesDeniedForRule", func(s socketio.Conn, data interface{}) {
+		server.BroadcastToRoom("/", "studiffy", "accesDeniedForRule", data)
 	})
 }
